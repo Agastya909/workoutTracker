361ec630d0f00b32e07eba05a28fd4380581fd7d
Agastya <agastya.r@pickmywork.com>
2024-01-15 17:58:08 +0530

    install packages and eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: "@react-native",
+  rules: {
+    quotes: 0,
+    curly: 0,
+    "react-native/no-unused-styles": 1,
+    "react-native/no-inline-styles": 0,
+    "react-native/split-platform-components": 2,
+    "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 2,
+    "react-native/sort-styles": 0,
+    "comma-dangle": [1, "never"],
+    "prettier/prettier": [1, { singleQuote: false, printWidth: 120, semi: true, trailingComma: false }],
+    "react/jsx-wrap-multilines": 2,
+    "react/jsx-props-no-multi-spaces": 0,
+    "react/jsx-max-props-per-line": 0,
+    "react/jsx-one-expression-per-line": 0
+  }
 };
